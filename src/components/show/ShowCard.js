b687cc76b838e3bbe98151a67a41bbf7f1e5d521
abhishek kotangale <abhishekkotangale2018@gmail.com>
2023-06-26 00:08:29 +0530

    created preview cards

---
+++ src/components/show/ShowCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const ShowCard = ({ name, image, id, summary, onStarMeClick, isStarred }) => {
+    const summaryAsText = summary
+    ? summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '') + '...'
+      : 'No description';
+  return (
+    <div>
+        <div>
+            <img src={image} alt='show' />
+        </div>
+        <h1>{name}</h1>
+        <p>{summaryAsText}</p>
+
+        <div>
+            <Link to={`/show/${id}`}>Read More</Link>
+            <button type='button'>Star Me</button>
+        </div>
+    </div>
+  );
+};
+
+export default ShowCard
