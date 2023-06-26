12482c9604484d5e8a31c752bd01444186380824
abhishek kotangale <abhishekkotangale2018@gmail.com>
2023-06-26 15:57:16 +0530

    added show page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
+import Show from './pages/Show';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/starred" element={<Starred />} />
+      <Route exact path="/show/:id" element={<Show/>}/>
       <Route path='*' element={<div>This is 404 page</div>} />
     </Routes>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,6 @@ const Home = () => {
 
   const isShowsSearch = searchOption ==='shows';
 
-
   const onSearch = () => {
 
     apiGet(`/search/${searchOption}?q=${input}`).then(result => {
